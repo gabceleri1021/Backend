975c1225abd1010c2b2219934bd0ce106d4ff4dc
gabceleri1021 <Gabriel.Pedreiro.Senai404@gmail.com>
2024-08-02 09:46:13 -0300

    Add files via upload

---
+++ PHP/index.php
@@ -0,0 +1,23 @@
+<?php
+// Declarei minhas variáveis para este exercício.
+$nome = 'Gabriel';
+$escola = 'SESI Cerquilho';
+$CE = 428 ;
+$cidade = 'Cerquiho';
+$curso = 'Desenvolvimento de Sistemas';
+$media = '9.5';
+
+//Irei imprimir o que o site da web deve aparecer.
+echo "Olá, bem vindo ao SESI SP <br>";
+echo "$nome <br>";
+echo "$escola <br>";
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>";
+
+// Resultado final com todas as variáveis juntos.
+echo "Olá $nome, bem vindo ao SESI $CE de $cidade, sua nota do $curso é: $media, parabéns. Muito obrigado por participar da $escola.";
+
+
+
+?>
