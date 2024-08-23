034d8fb2c0952be643e8f188a3f72f96ea9815fd
gabceleri1021 <Gabriel.Pedreiro.Senai404@gmail.com>
2024-08-23 09:09:19 -0300

    Add files via upload

---
+++ PHP/exfor.php
@@ -0,0 +1,10 @@
+<?php
+
+for ($i=1; $i <=5; $i++) {
+    echo " O valor de i é $i<br>";
+}
+
+
+
+
+?>
--- PHP/index.php
+++ PHP/index.php
@@ -1,23 +1,22 @@
 <?php
-// Declarei minhas variáveis para este exercício.
-$nome = 'Gabriel';
-$escola = 'SESI Cerquilho';
-$CE = 428 ;
-$cidade = 'Cerquiho';
+
+ /*Declarando Variáveis*/
+
+$nome = ' Emanuela';
+$cidade = 'Cerquilho';
+$CE = 428;
 $curso = 'Desenvolvimento de Sistemas';
-$media = '9.5';
+$media =9.5;
 
-//Irei imprimir o que o site da web deve aparecer.
-echo "Olá, bem vindo ao SESI SP <br>";
-echo "$nome <br>";
-echo "$escola <br>";
-echo "$CE <br>";
-echo "$curso <br>";
-echo "$media <br>";
+/*Chamando variável?*/
 
-// Resultado final com todas as variáveis juntos.
-echo "Olá $nome, bem vindo ao SESI $CE de $cidade, sua nota do $curso é: $media, parabéns. Muito obrigado por participar da $escola.";
+echo "$nome  <br>"; 
+echo "$cidade  <br>" ; 
+echo "$CE  <br>" ; 
+echo "$curso  <br>" ; 
+echo "$media   <br>"; 
 
 
+echo "Olá $nome, bem vindo ao Sesi $cidade" //Exemplo//
 
 ?>
